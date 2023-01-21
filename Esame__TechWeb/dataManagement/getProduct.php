2fd9de1ad7497572ff81f96427ebc7d838bc431e
eleonora <eleonorafil21@gmail.com>
2023-01-21 16:21:11 +0100

    aaa

--- Esame__TechWeb/dataManagement/getProduct.php
+++ Esame__TechWeb/dataManagement/getProduct.php
@@ -3,8 +3,8 @@
 //visualizzazione dei prodotti esistenti per una categoria. Proprio da quella funzione viene
 //passata qui la categoria di prodotti, e a sua volta questo file passerà i dati alla funzione
 //getMobili presente nel file db.php, che recupererà effettivamente tutte le informazioni sui
-//singoli prodotti per categoria dal db. Ottenuta questa informazione questo file stamperà il contenuto json
-//elaborato dalla richiesta al db.
+//singoli prodotti per categoria dal db. Ottenuta questa informazione questo invierà il contenuto json
+//elaborato dalla richiesta al db al file homepage.js che lo stamperà.
 include "db.php";
 header("Content-type: application/json");
 //questa funzione header invia l'intestazione http json al browser
